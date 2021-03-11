100009548964b53853aca9ef906fe00a48a225a6
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-11 14:32:25 +0100

    Updates

---
+++ precache-manifest.b0d0a772aee82863595192787b88f237.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1401adc386af7fa36e7af91411fa4f12",
+    "url": "/mern-user/index.html"
+  },
+  {
+    "revision": "90ac6971297b07d919e0",
+    "url": "/mern-user/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "ded61b0050c55d7a3850",
+    "url": "/mern-user/static/css/main.bd5e3db0.chunk.css"
+  },
+  {
+    "revision": "90ac6971297b07d919e0",
+    "url": "/mern-user/static/js/2.ca9c084c.chunk.js"
+  },
+  {
+    "revision": "8f9a70a746088073fceab4989079aa3c",
+    "url": "/mern-user/static/js/2.ca9c084c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ded61b0050c55d7a3850",
+    "url": "/mern-user/static/js/main.8417c664.chunk.js"
+  },
+  {
+    "revision": "e3a0a29a33b51c8cc8ea",
+    "url": "/mern-user/static/js/runtime-main.f2bdeb38.js"
+  }
+]);
